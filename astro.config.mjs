7aeef638f71cc627a59684fbfb42b5fde991bbad
mitchparker99 <mitchelljamesparker99@gmail.com>
2024-08-07 19:45:11 +1000

    update all

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig } from 'astro/config';
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astro-multiverse.vercel.app',
-})
+// # site: 'https://astro-multiverse.vercel.app',
+  output: "server",
+  adapter: netlify(),
+});
